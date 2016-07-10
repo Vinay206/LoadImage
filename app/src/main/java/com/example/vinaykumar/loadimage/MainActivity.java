55e6d843c2e9cdce0e77274c7ac107c72fcfb78f
Vinay206 <k.vinaykumar51@gmail.com>
2016-07-10 13:38:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vinaykumar/loadimage/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.vinaykumar.loadimage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello toast!", Toast.LENGTH_SHORT);
+
+
+        Button buttonLoadImage = (Button) findViewById(R.id.button);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+
+            }
+        });
+
+        toast.setText("loaded");
+        toast.show();
+        
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            //imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            imageView.setImageURI(Uri.parse(picturePath));
+
+
+            Toast toast = Toast.makeText(getApplicationContext(), "imgloaded", Toast.LENGTH_LONG);
+            toast.setText(picturePath.toString());
+            toast.show();
+
+
+
+
+        }
+
+
+    }
+
+}
